01009811b4d8b651cafeed6f2bd191b76a4629eb
wang-hub <1679061394@qq.com>
2020-04-26 20:09:33 +0800

    学习使用
    小伙伴们都可以加入自己的东西

---
+++ github_learn.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 26 20:06:16 2020
+
+hello,welcome to github
+
+在下面加上一行print语句
+打印出自己的姓名
+也可以加点自己想说的东西（最好一人一个print）
+
+（这句话用过英语怎么说= =）
+
+@author: wangwei
+"""
+
+print("wangwei:","hello,you can call me paixiaoxing,i am the first to here!i am a real man = =")
